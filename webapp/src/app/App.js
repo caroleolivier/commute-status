506e3d57b4eb3327e253e96064ee9e419f1a1521
carole.olivier <carole.olivier@gmail.com>
2017-10-13 17:28:14 +0100

    Add routing

--- webapp/src/app/App.js
+++ webapp/src/app/App.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import config from '../config/config';
 
 import styles from './App.scss';
 
-import Header from './Header';
+import ErrorMessage from '../common/ErrorMessage';
+import RouteDirectionSelector from '../routeSelector/RouteSelector';
+import Header from '../route/Header';
 import CommuteRoute from '../route/CommuteRoute';
 
 class App extends Component {
-    constructor(prop) {
-        super(prop);
-        this.state = {
-            direction: 'fromHome'
-        };
-    }
-
     render() {
-        const routes = config[this.state.direction];
-        const routesComponents = routes.map(route =>
-            <CommuteRoute key={route.routeName} routeName={route.routeName} stops={route.stops} />);
+        let content;
+        if (this.props.urlArg in config) {
+            const direction = this.props.urlArg;
+            const routes = config[this.props.urlArg];
+            const routesComponents = routes.map(route =>
+                <CommuteRoute key={route.routeName} routeName={route.routeName} stops={route.stops} />);
+            content = (
+                <div className={styles.App}>
+                    <Header direction={direction} />
+                    <div>
+                        {routesComponents}
+                    </div>
+                </div>
+            );
+        } else if (this.props.urlArg === '') {
+            content = <RouteDirectionSelector />;
+        } else {
+            content = (
+                <div className={styles.ErrorMessage}>
+                    <ErrorMessage className={styles.ErrorMessage} message="Unknown URL" />
+                </div>
+            );
+        }
+
         return (
             <div className={styles.App}>
-                <Header direction={this.state.direction} />
-                <div>
-                    {routesComponents}
-                </div>
+                {content}
             </div>
         );
     }
 }
+
+App.propTypes = {
+    urlArg: PropTypes.string
+};
+App.defaultProps = {
+    urlArg: ''
+};
 
 export default App;
--- webapp/src/main.js
+++ webapp/src/main.js
@@ -5,7 +5,15 @@ import ReactDOM from 'react-dom';
 
 import App from './app/App';
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById('mount'),
-);
+
+function handleNewHash() {
+    const urlArgs = window.location.hash.replace(/^#\/?|\/$/g, '').split('/');
+    ReactDOM.render(
+        React.createElement(App, { urlArg: urlArgs[0] }),
+        document.getElementById('mount'),
+    );
+}
+
+// Handle the initial route and browser navigation events
+handleNewHash();
+window.addEventListener('hashchange', handleNewHash, false);

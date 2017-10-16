506e0f1dbda199cc98b57bbe4b2f7e58d31b6007
carole.olivier <carole.olivier@gmail.com>
2017-10-16 12:20:49 +0100

    Add config for to home route

--- webapp/src/config/config.js
+++ webapp/src/config/config.js
@@ -16,7 +16,7 @@ const config = {
                     stationName: 'Tooting Broadway',
                     stationId: '940GZZLUTBY',
                     direction: 'Central London',
-                    directionId: 'inbound',
+                    directionId: 'outbound',
                     lines: ['northern']
                 }
             ]
@@ -32,14 +32,14 @@ const config = {
                     directionId: 'inbound',
                     lines: ['77', '44', '270']
                 },
-                // {
-                //     type: 'train',
-                //     stationName: 'Earlsfield',
-                //     stationId: '',
-                //     direction: '',
-                //     directionId: '',
-                //     lines: ['']
-                // },
+                {
+                    type: 'train',
+                    stationName: 'Earlsfield',
+                    stationId: '',
+                    direction: '',
+                    directionId: '',
+                    lines: ['']
+                },
                 {
                     type: 'tube',
                     stationName: 'Waterloo',
@@ -47,6 +47,58 @@ const config = {
                     direction: 'Bank',
                     directionId: 'outbound',
                     lines: ['waterloo-city']
+                }
+            ]
+        }
+    ],
+    toHome: [
+        {
+            routeName: 'Via Waterloo',
+            stops: [
+                {
+                    type: 'tube',
+                    stationName: 'Bank',
+                    stationId: '940GZZLUBNK',
+                    direction: 'Waterloo',
+                    directionId: 'inbound',
+                    lines: ['waterloo-city']
+                },
+                {
+                    type: 'train',
+                    stationName: 'Waterloo',
+                    stationId: '',
+                    direction: '',
+                    directionId: '',
+                    lines: ['']
+                },
+                {
+                    type: 'bus',
+                    stationName: 'Earlsfield',
+                    stationId: '490006306D',
+                    direction: 'Tooting Broadway',
+                    directionId: 'outbound',
+                    lines: ['77', '44', '270']
+                }
+            ]
+        },
+        {
+            routeName: 'Via Tooting Broadway',
+            stops: [
+                {
+                    type: 'tube',
+                    stationName: 'Bank',
+                    stationId: '940GZZLUBNK',
+                    direction: 'Tooting Broadway',
+                    directionId: 'inbound',
+                    lines: ['northern']
+                },
+                {
+                    type: 'bus',
+                    stationName: 'Tooting Broadway',
+                    stationId: '490000234H',
+                    direction: 'Earlsfield',
+                    directionId: 'inbound',
+                    lines: ['77', '44', '270']
                 }
             ]
         }

50ef61341787b28b1567dde228ab1bacf5981770
caroleolivier <carole.olivier@gmail.com>
2017-10-26 18:42:15 +0100

    Improve API

--- backend/train/LDB/LDB/Controllers/LiveArrivalDataController.cs
+++ backend/train/LDB/LDB/Controllers/LiveArrivalDataController.cs
@@ -1,7 +1,9 @@
-﻿using LDB.Model;
+﻿using LDB.Helper;
+using LDB.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using OpenLDBWS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,14 +22,17 @@ namespace LDB.Controllers
 
         // http://<servername>:<port>/api/live/arrivals/EAD/WAT/to/10/0/30
         [HttpGet("{at}/{destination}/{filterdirection}/{resultno}/{offset}/{window}")]
-        public async Task<IEnumerable<TrainArrival>> Get(string at, string destination, FilterType filterdirection, ushort resultno, int offset, int window)
+        public async Task<IEnumerable<ArrivalTime>> Get(string at, string destination, FilterType filterdirection, ushort resultno, int offset, int window)
         {
             var client = new LDBServiceSoapClient(new LDBServiceSoapClient.EndpointConfiguration(), _serviceConfig.Address);
             var token = new AccessToken() { TokenValue = _serviceConfig.Key };
 
             GetArrivalBoardResponse res = await client.GetArrivalBoardAsync(token, resultno, at, destination, filterdirection, offset, window);
 
-            return res.GetStationBoardResult.trainServices.Select(ts => new TrainArrival(at, ts.sta, ts.eta, string.Join(",", ts.destination.Select(d => d.crs)))).ToArray();
+            return res.GetStationBoardResult.trainServices
+                .Select(item => item.ToTimeArrival(at))
+                .Where(ta => ta.DestinationCode.Contains(destination)) // there is a bug in the API I think where it returns trains that should be filtered
+                .ToArray();
         }
     }
 }
---
+++ backend/train/LDB/LDB/Model/ArrivalTime.cs
@@ -0,0 +1,39 @@
+﻿namespace LDB.Model
+{
+    public sealed class ArrivalTime
+    {
+        public ArrivalTime(string atStation, string destinationCode, string lineName, int expectedSecs, string expectedTime)
+        {
+            AtStation = atStation;
+            DestinationCode = destinationCode;
+            LineName = lineName;
+            ExpectedSecs = expectedSecs;
+            ExpectedTime = expectedTime;
+        }
+
+        /// <summary>
+        /// Station code we are interested in.
+        /// </summary>
+        public string AtStation { get; }
+
+        /// <summary>
+        /// Final destination code.
+        /// </summary>
+        public string DestinationCode { get; }
+
+        /// <summary>
+        /// The train operator for that train.
+        /// </summary>
+        public string LineName { get; }
+
+        /// <summary>
+        /// Expected arrival in Seconds.
+        /// </summary>
+        public int ExpectedSecs { get; }
+
+        /// <summary>
+        /// The expected timere returned by datasource.
+        /// </summary>
+        public string ExpectedTime { get; }
+    }
+}
